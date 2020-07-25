aca66d681c489f38f152e53a3780bdbab9c16274
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-25 18:08:45 +0600

    element selected & data declared

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('connected')
+const displayPlayer1Score = document.getElementById('player1Score');
+const displayPlayer2Score = document.getElementById('player2Score');
+
+const displayWinScore = document.querySelector('p span');
+const inputScore = document.getElementById('input');
+const player1Btn = document.getElementById('player1Btn');
+const player2Btn = document.getElementById('player2Btn');
+const resetBtn = document.getElementById('reset')
+
+const p1Score = 0;
+const p2Score = 0;
+const winningScore = 5;
+
+player1Btn.addEventListener('click', () => {
+    console.log('1')
+})
+
+player2Btn.addEventListener('click', () => {
+    console.log('2')
+})
